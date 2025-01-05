95dfd07972b30b97409095ccb13326cd87269de8
graysuit <54432616+graysuit@users.noreply.github.com>
2025-01-05 20:31:51 +0500

    Add files via upload

---
+++ src/MainForm.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CodeList = New System.Windows.Forms.TextBox()
+        Me.StopBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.StartBtn = New System.Windows.Forms.Button()
+        Me.InfoLabel = New System.Windows.Forms.Label()
+        Me.LinkSlimjet = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'CodeList
+        '
+        Me.CodeList.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CodeList.Location = New System.Drawing.Point(13, 39)
+        Me.CodeList.Margin = New System.Windows.Forms.Padding(4)
+        Me.CodeList.MaxLength = 999999999
+        Me.CodeList.Multiline = True
+        Me.CodeList.Name = "CodeList"
+        Me.CodeList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.CodeList.Size = New System.Drawing.Size(476, 314)
+        Me.CodeList.TabIndex = 0
+        '
+        'StopBtn
+        '
+        Me.StopBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.StopBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.StopBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.StopBtn.Location = New System.Drawing.Point(389, 361)
+        Me.StopBtn.Margin = New System.Windows.Forms.Padding(4)
+        Me.StopBtn.Name = "StopBtn"
+        Me.StopBtn.Size = New System.Drawing.Size(100, 28)
+        Me.StopBtn.TabIndex = 1
+        Me.StopBtn.Text = "Stop"
+        Me.StopBtn.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(10, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "ASIN List"
+        '
+        'StartBtn
+        '
+        Me.StartBtn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.StartBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.StartBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.StartBtn.Location = New System.Drawing.Point(389, 361)
+        Me.StartBtn.Margin = New System.Windows.Forms.Padding(4)
+        Me.StartBtn.Name = "StartBtn"
+        Me.StartBtn.Size = New System.Drawing.Size(100, 28)
+        Me.StartBtn.TabIndex = 3
+        Me.StartBtn.Text = "Start"
+        Me.StartBtn.UseVisualStyleBackColor = False
+        '
+        'InfoLabel
+        '
+        Me.InfoLabel.AutoSize = True
+        Me.InfoLabel.BackColor = System.Drawing.Color.Transparent
+        Me.InfoLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.InfoLabel.Location = New System.Drawing.Point(81, 18)
+        Me.InfoLabel.Name = "InfoLabel"
+        Me.InfoLabel.Size = New System.Drawing.Size(41, 17)
+        Me.InfoLabel.TabIndex = 4
+        Me.InfoLabel.Text = "[0/0]"
+        '
+        'LinkSlimjet
+        '
+        Me.LinkSlimjet.AutoSize = True
+        Me.LinkSlimjet.LinkColor = System.Drawing.Color.Red
+        Me.LinkSlimjet.Location = New System.Drawing.Point(10, 367)
+        Me.LinkSlimjet.Name = "LinkSlimjet"
+        Me.LinkSlimjet.Size = New System.Drawing.Size(49, 17)
+        Me.LinkSlimjet.TabIndex = 5
+        Me.LinkSlimjet.TabStop = True
+        Me.LinkSlimjet.Text = "Slimjet"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(502, 402)
+        Me.Controls.Add(Me.LinkSlimjet)
+        Me.Controls.Add(Me.StartBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StopBtn)
+        Me.Controls.Add(Me.CodeList)
+        Me.Controls.Add(Me.InfoLabel)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Amazon Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CodeList As TextBox
+    Friend WithEvents StopBtn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents StartBtn As Button
+    Friend WithEvents InfoLabel As Label
+    Friend WithEvents LinkSlimjet As LinkLabel
+End Class
---
+++ src/MainForm.vb
@@ -0,0 +1,84 @@
+﻿Imports System.IO
+Imports System.Reflection
+Imports System.Text
+Imports OpenQA.Selenium
+Imports OpenQA.Selenium.Chrome
+Imports SeleniumUndetectedChromeDriver
+
+Public Class MainForm
+    Dim ShouldStop As Boolean = False
+    Dim Slimjet = "C:\Program Files\Slimjet\slimjet.exe"
+    Private Sub StopBtn_Click(sender As Object, e As EventArgs) Handles StopBtn.Click
+        ShouldStop = True
+    End Sub
+    Dim Alpha As Char() = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-={}|[]\`~:"";'?><,./".ToArray
+    Function Clean(Name As String) As String
+        Dim Out = String.Empty
+        If String.IsNullOrEmpty(Name) Then Return Out
+        For Each N In Name.ToLower()
+            If Alpha.Contains(N) Then
+                Out &= N
+            Else
+                Out &= " "
+            End If
+        Next
+        While Out.Contains("  ")
+            Out = Out.Replace("  ", " ")
+        End While
+        Return Out.TrimStart(" ").TrimEnd(" ")
+    End Function
+
+    Async Sub StartBtn_Click(sender As Object, e As EventArgs) Handles StartBtn.Click
+        setStatus()
+        ShouldStop = False
+        StartBtn.Visible = False
+        Await Task.Run(
+            Sub()
+                Try
+                    Dim Codes = CodeList.Lines.Where(Function(C) Not String.IsNullOrEmpty(C))
+                    Dim Options As New ChromeOptions()
+                    Options.AddArgument($"--hide-crash-restore-bubble")
+                    Options.AddArgument("--incognito")
+                    Options.AddArgument("--start-maximized")
+                    Options.AddArgument("--blink-settings=imagesEnabled=false")
+                    Options.PageLoadStrategy = PageLoadStrategy.Eager
+                    Options.BinaryLocation = Slimjet
+
+                    Using Driver = UndetectedChromeDriver.Create(Options, Nothing, Path.GetFullPath("chromedriver.exe"), Slimjet, hideCommandPromptWindow:=True, commandTimeout:=TimeSpan.FromHours(1))
+                        Using sw As New StreamWriter("Output.csv", False, Encoding.Unicode)
+                            sw.WriteLine(String.Join(vbTab, New String() {"ASIN", "isValid", "Title"}))
+                            For index = 0 To Codes.Count - 1
+                                If ShouldStop Then Exit For
+                                Dim ASIN = Codes.ElementAt(index)
+                                Driver.Navigate.GoToUrl($"https://www.amazon.com/dp/{ASIN}")
+                                Dim Title = Clean(Driver.Title)
+                                setInfo($"[{index + 1}/{ Codes.Count}] {ASIN} {Title}")
+                                Dim isValid = Not Title.Contains("not found")
+                                sw.WriteLine(String.Join(vbTab, New String() {ASIN, isValid, Title}))
+                            Next
+                        End Using
+                    End Using
+                    setInfo("Finished scraping")
+                Catch ex As Exception
+                    setInfo($"Error:{ex.Message}")
+                End Try
+            End Sub)
+        StartBtn.Visible = True
+    End Sub
+
+    Sub setInfo(info)
+        InfoLabel.Invoke(Sub() InfoLabel.Text = info)
+    End Sub
+
+    Private Sub LinkSlimjet_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkSlimjet.LinkClicked
+        Process.Start("https://www.slimjet.com/release/archive/38.0.9.0/sjtsetup_x64.exe")
+    End Sub
+    Sub setStatus()
+        Dim isSlimjetInstalled = File.Exists(Slimjet)
+        LinkSlimjet.LinkColor = If(isSlimjetInstalled, Color.Green, Color.Red)
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        setStatus()
+    End Sub
+End Class
